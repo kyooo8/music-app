dc0ee53e067eb5998427d0566ecfb330dd6e2b80
kyooo8 <kyosuke.7876@gmail.com>
2024-12-19 12:21:00 +0900

    同様

--- app/index.tsx
+++ app/index.tsx
@@ -12,34 +12,22 @@ export default function Index() {
   const { setIsLogin } = useContext(LoginContext);
   const localToFirestorage = async (projects: Project[]) => {
     if (isSynced) return;
-
-    console.log(projects);
-
     const userUid = auth.currentUser?.uid;
     if (!userUid) return;
     for (const project of projects) {
       await addDoc(collection(db, `users/${userUid}/projects`), { ...project });
     }
-    console.log("All local projects are written to Firestore");
     await AsyncStorage.removeItem("projects");
 
     isSynced = true;
   };
   useEffect(() => {
-    console.log("useEffect triggered");
-
     const unsubscribe = onAuthStateChanged(auth, async (user) => {
-      console.log("onAuthStateChanged triggered", user);
-
       if (user) {
         setIsLogin(true);
         const storedData = await AsyncStorage.getItem("projects");
-        console.log("Stored data:", storedData);
-
         if (storedData) {
           const projects = JSON.parse(storedData) as Project[];
-          console.log("Projects to sync:", projects);
-
           if (projects.length > 0) {
             await localToFirestorage(projects);
           }
